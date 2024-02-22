67cded75fd6cf38d525ac2e3c054ffd7f940f1fe
cole <bernankeic@gmail.com>
2024-02-22 17:26:42 +0800

    docs: perf demo style

--- playground/src/demos/Collapse.vue
+++ playground/src/demos/Collapse.vue
@@ -3,7 +3,7 @@
     <Collapse title="Collapse Title" desc="short description">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus aliquam sodales. Donec vitae malesuada nibh. Duis congue nibh at dolor efficitur, id euismod elit condimentum. Maecenas commodo odio at velit auctor vehicula. Nam lorem tellus, ornare eget luctus ac, scelerisque a nibh. Aliquam erat volutpat. Proin egestas turpis orci, eget aliquet leo scelerisque in. Vestibulum a sagittis ipsum. Nullam sit amet sollicitudin ex. Nulla egestas leo in ligula tincidunt egestas. Nam ac leo urna. Nunc venenatis pellentesque ipsum in convallis. Quisque dignissim nibh a elit consequat, quis porta sem efficitur. Duis finibus velit sit amet urna blandit venenatis. Aliquam eget rutrum lorem.
     </Collapse>
-    <Collapse class="bg-lightblue-50!" :duration="0.5">
+    <Collapse class="bg-lightblue-50 dark:bg-lightblue-900" :duration="0.5">
       <template #summary="{ collapsed }">
         <div class="w-full flex items-center justify-between">
           <div class="flex items-center gap-1">
