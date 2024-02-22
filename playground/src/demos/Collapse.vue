ed053d799917c6987c5d00c3e329bb9a7643ef26
cole <bernankeic@gmail.com>
2024-02-22 17:19:28 +0800

    feat: update Collapse style

--- playground/src/demos/Collapse.vue
+++ playground/src/demos/Collapse.vue
@@ -1,7 +1,21 @@
 <template>
-  <Collapse title="Collapse Title" desc="short description">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus aliquam sodales. Donec vitae malesuada nibh. Duis congue nibh at dolor efficitur, id euismod elit condimentum. Maecenas commodo odio at velit auctor vehicula. Nam lorem tellus, ornare eget luctus ac, scelerisque a nibh. Aliquam erat volutpat. Proin egestas turpis orci, eget aliquet leo scelerisque in. Vestibulum a sagittis ipsum. Nullam sit amet sollicitudin ex. Nulla egestas leo in ligula tincidunt egestas. Nam ac leo urna. Nunc venenatis pellentesque ipsum in convallis. Quisque dignissim nibh a elit consequat, quis porta sem efficitur. Duis finibus velit sit amet urna blandit venenatis. Aliquam eget rutrum lorem.
-  </Collapse>
+  <div class="flex flex-col gap-3">
+    <Collapse title="Collapse Title" desc="short description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus aliquam sodales. Donec vitae malesuada nibh. Duis congue nibh at dolor efficitur, id euismod elit condimentum. Maecenas commodo odio at velit auctor vehicula. Nam lorem tellus, ornare eget luctus ac, scelerisque a nibh. Aliquam erat volutpat. Proin egestas turpis orci, eget aliquet leo scelerisque in. Vestibulum a sagittis ipsum. Nullam sit amet sollicitudin ex. Nulla egestas leo in ligula tincidunt egestas. Nam ac leo urna. Nunc venenatis pellentesque ipsum in convallis. Quisque dignissim nibh a elit consequat, quis porta sem efficitur. Duis finibus velit sit amet urna blandit venenatis. Aliquam eget rutrum lorem.
+    </Collapse>
+    <Collapse class="bg-lightblue-50!" :duration="0.5">
+      <template #summary="{ collapsed }">
+        <div class="w-full flex items-center justify-between">
+          <div class="flex items-center gap-1">
+            Another Style
+            <div class="i-lucide:chevron-right transition" :class="[!collapsed ? 'rotate-90' : '']"></div>
+          </div>
+          <div>Suffix</div>
+        </div>
+      </template>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus aliquam sodales. Donec vitae malesuada nibh. Duis congue nibh at dolor efficitur, id euismod elit condimentum. Maecenas commodo odio at velit auctor vehicula. Nam lorem tellus, ornare eget luctus ac, scelerisque a nibh. Aliquam erat volutpat. Proin egestas turpis orci, eget aliquet leo scelerisque in. Vestibulum a sagittis ipsum. Nullam sit amet sollicitudin ex. Nulla egestas leo in ligula tincidunt egestas. Nam ac leo urna. Nunc venenatis pellentesque ipsum in convallis. Quisque dignissim nibh a elit consequat, quis porta sem efficitur. Duis finibus velit sit amet urna blandit venenatis. Aliquam eget rutrum lorem.
+    </Collapse>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/components/Collapse.vue
+++ src/components/Collapse.vue
@@ -4,8 +4,8 @@
     :class="[collapsed ? 'p-b-0 grid-rows-[auto_0fr]' : 'p-b-5 grid-rows-[auto_1fr]']"
   >
     <div class="z-1 flex cursor-pointer justify-between p-5" @click="collapsed = !collapsed">
-      <div>
-        <slot name="summary">
+      <div class="w-full">
+        <slot name="summary" :collapsed="collapsed">
           {{ title }}
           <span class="m-l-2 align-end text-3.5 text-neutral-400">{{ desc }}</span>
         </slot>
@@ -13,32 +13,53 @@
       <div class="i-icon-park-outline:right text-5 transition" :class="[collapsed ? '' : 'rotate-90']"></div>
     </div>
     <div class="content p-x-5" :class="[collapsed ? 'opacity-0 min-h-0' : 'min-h-fit']">
-      <slot></slot>
+      <slot :collapsed="collapsed"></slot>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { useMergedState } from "../composables/useMergedState";
 
-defineProps<{
+const props = withDefaults(defineProps<{
+  defaultCollapsed?: boolean;
+  modelValue?: boolean;
+  duration?: number | string;
   title?: string;
   desc?: string;
+}>(), {
+  modelValue: undefined,
+  defaultCollapsed: true,
+  duration: 0.2,
+});
+
+const emit = defineEmits<{
+  "update:modelValue": [collapsed:boolean];
 }>();
 
-const collapsed = ref(true);
+const controlled = computed({
+  get: () => props.modelValue,
+  set: c => c && emit("update:modelValue", c),
+});
+
+const uncontrolled = ref(props.defaultCollapsed);
+
+const duration = computed(() => typeof props.duration === "number" ? `${props.duration}s` : props.duration);
+
+const collapsed = useMergedState(controlled, uncontrolled);
 </script>
 
 <style scoped>
 .collapse {
   transition-timing-function: ease-in-out;
-  transition-duration: 0.2s;
+  transition-duration: v-bind(duration);
   transition-property: padding, grid-template-rows;
 }
 
 .content {
   transition-timing-function: ease-in-out;
-  transition-duration: 0.2s;
+  transition-duration: v-bind(duration);
   transition-property: opacity;
 }
 </style>
